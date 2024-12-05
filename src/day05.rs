1c39f6f32dda23a4e6d854c386c264779c1f3ac5
Jayson Lennon <jayson@jaysonlennon.dev>
2024-12-05 13:10:44 -0800

    add day5

--- src/day05.rs
+++ src/day05.rs
@@ -1,34 +1,44 @@
-use std::{
-    cmp::Ordering,
-    collections::{HashMap, HashSet},
-};
+use std::{cmp::Ordering, collections::HashMap};
 
 use crate::AocSolver;
 
 pub struct Day5Solver;
+
+// Strategy: Implement PartialEq, Eq, PartialOrd, and Ord using the page ordering rules.
+// Then use std sort.
 
 impl AocSolver for Day5Solver {
     type Output = u32;
 
     fn part_1(input: &str) -> Self::Output {
         let manual = SafetyManual::from(input);
-        let updates = manual.iter_valid_part1_updates().collect::<Vec<_>>();
+        let updates = manual.iter_updates(Sorted::Correctly);
 
-        let mut sum: u32 = 0;
-        for update in updates {
-            let mid_index = update.len() / 2;
-            sum += update[mid_index] as u32;
-        }
-        sum
+        median(updates)
     }
 
     fn part_2(input: &str) -> Self::Output {
-        0
+        let manual = SafetyManual::from(input);
+        let updates = manual.iter_updates(Sorted::Incorrectly).map(|update| {
+            let mut update = update.clone();
+            update.sort();
+            update
+        });
+
+        median(updates)
     }
 }
 
-type Rules = HashMap<u8, Vec<u8>>;
-type Updates = Vec<Vec<u8>>;
+fn median(updates: impl Iterator<Item = Vec<Page>>) -> u32 {
+    let mut sum: u32 = 0;
+    for update in updates {
+        let mid_index = update.len() / 2;
+        sum += update[mid_index].number as u32;
+    }
+    sum
+}
+
+type Rules = HashMap<u8, Page>;
 
 #[derive(Debug, Clone)]
 struct Page {
@@ -39,6 +49,7 @@ struct Page {
 }
 
 impl Page {
+    /// Create a new empty page. Used as a default if there are no rules applied to the page.
     fn empty(page: u8) -> Self {
         Self {
             number: page,
@@ -46,6 +57,8 @@ impl Page {
         }
     }
 }
+
+// Implement std stuff
 
 impl Eq for Page {}
 
@@ -75,85 +88,56 @@ impl Ord for Page {
     }
 }
 
+/// For the `iter_updates` method. Determines if we want to get back only sorted or only unsorted
+/// entries
+#[derive(Clone, Copy)]
+enum Sorted {
+    Correctly,
+    Incorrectly,
+}
+
+/// Contains the rules and updates parsed from the problem input.
 #[derive(Debug)]
 struct SafetyManual {
     rules: Rules,
-    updates: Updates,
+    updates: Vec<Vec<u8>>,
 }
 
 impl SafetyManual {
-    fn rules(&self) -> &Rules {
-        &self.rules
-    }
-    fn updates(&self) -> &Updates {
-        &self.updates
-    }
-
-    /// Returns part1 valid updates
-    fn iter_valid_part1_updates(&self) -> impl Iterator<Item = &Vec<u8>> {
-        self.updates.iter().filter(|pages| {
-            let mut order_buf: Vec<u8> = Vec::default();
-            for page in pages.iter() {
-                if order_buf.contains(page) {
-                    return false;
+    /// Returns an iterator over updates in `Page` format so they can be sorted later.
+    fn iter_updates(&self, sorted: Sorted) -> impl Iterator<Item = Vec<Page>> + '_ {
+        use Sorted::*;
+        self.updates.iter().filter_map(move |pages| {
+            let update = pages
+                .iter()
+                .map(|page| {
+                    self.rules
+                        .get(page)
+                        .cloned()
+                        .unwrap_or_else(|| Page::empty(*page))
+                })
+                .collect::<Vec<_>>();
+            match sorted {
+                Correctly => {
+                    if update.is_sorted() {
+                        Some(update)
+                    } else {
+                        None
+                    }
                 }
-                order_buf.push(*page);
-                if let Some(follows) = self.rules.get(page) {
-                    for p in follows {
-                        if order_buf.contains(p) {
-                            return false;
-                        }
+                Incorrectly => {
+                    if !update.is_sorted() {
+                        Some(update)
+                    } else {
+                        None
                     }
                 }
             }
-            true
         })
-    }
-
-    fn iter_incorrectly_sorted(&self) -> impl Iterator<Item = &Vec<u8>> {
-        self.updates.iter().filter(|pages| {
-            let mut order_buf: Vec<u8> = Vec::default();
-            for page in pages.iter() {
-                if order_buf.contains(page) {
-                    return false;
-                }
-                order_buf.push(*page);
-                if let Some(follows) = self.rules.get(page) {
-                    for p in follows {
-                        if order_buf.contains(p) {
-                            return true;
-                        }
-                    }
-                }
-            }
-            false
-        })
-    }
-
-    fn sort(&self, incorrect_updates: &[u8]) -> Vec<Page> {
-        let pages = self
-            .rules
-            .iter()
-            .map(|(page, follows)| {
-                (
-                    *page,
-                    Page {
-                        number: *page,
-                        follows: follows.to_owned(),
-                    },
-                )
-            })
-            .collect::<HashMap<u8, Page>>();
-
-        let mut pages = incorrect_updates
-            .iter()
-            .map(|n| pages.get(n).cloned().unwrap_or_else(|| Page::empty(*n)))
-            .collect::<Vec<_>>();
-        pages.sort();
-        pages
     }
 }
 
+/// Parser helper
 #[derive(Debug, PartialEq, Eq)]
 enum ParseType {
     Rule,
@@ -179,8 +163,8 @@ impl From<&str> for SafetyManual {
                         let (page, follows) = line.split_once('|').unwrap();
                         (page.parse::<u8>().unwrap(), follows.parse::<u8>().unwrap())
                     };
-                    let entry: &mut Vec<u8> = rules.entry(page).or_default();
-                    entry.push(follows);
+                    let entry = rules.entry(page).or_insert_with(|| Page::empty(page));
+                    entry.follows.push(follows);
                 }
                 PrintOrder => {
                     updates.push(line.split(',').map(|n| n.parse::<u8>().unwrap()).collect());
@@ -225,24 +209,23 @@ mod tests {
 97,13,75,29,47"#;
 
     #[test]
-    fn parses_into_safety_manual() {
-        let manual = SafetyManual::from(SAMPLE);
-
-        assert_eq!(manual.rules().len(), 6);
-        assert_eq!(manual.updates().len(), 6);
-
-        assert_eq!(manual.rules()[&47], &[53, 13, 61, 29]);
-        assert_eq!(manual.updates()[0], &[75, 47, 61, 53, 29]);
-    }
-
-    #[test]
     fn get_correctly_ordered_updates() {
         let manual = SafetyManual::from(SAMPLE);
 
-        let updates = manual.iter_valid_part1_updates().collect::<Vec<_>>();
+        let updates = manual
+            .iter_updates(Sorted::Correctly)
+            .map(|update| {
+                update
+                    .into_iter()
+                    .map(|page| page.number)
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
 
         assert_eq!(updates.len(), 3);
         assert_eq!(updates[0], &[75, 47, 61, 53, 29]);
+        assert_eq!(updates[1], &[97, 61, 53, 29, 13]);
+        assert_eq!(updates[2], &[75, 29, 13]);
     }
 
     #[test]
@@ -253,35 +236,28 @@ mod tests {
     }
 
     #[test]
-    fn get_incorrectly_sorted_entries() {
+    fn get_incorrectly_ordered_updates() {
         let manual = SafetyManual::from(SAMPLE);
 
-        let incorrect_updates = manual.iter_incorrectly_sorted().collect::<Vec<_>>();
-        assert_eq!(incorrect_updates.len(), 3);
-        assert_eq!(incorrect_updates[0], &[75, 97, 47, 61, 53]);
-    }
-
-    #[test]
-    fn applies_sort() {
-        let manual = SafetyManual::from(SAMPLE);
-
-        let incorrect_updates = manual.iter_incorrectly_sorted().collect::<Vec<_>>();
-
-        let sorted = incorrect_updates
-            .into_iter()
-            .map(|pages| {
-                manual
-                    .sort(pages)
+        let updates = manual
+            .iter_updates(Sorted::Incorrectly)
+            .map(|update| {
+                update
                     .into_iter()
                     .map(|page| page.number)
                     .collect::<Vec<_>>()
             })
             .collect::<Vec<_>>();
 
-        assert_eq!(sorted[0], vec![97, 75, 47, 61, 53]);
-        assert_eq!(sorted[1], vec![61, 29, 13]);
-        assert_eq!(sorted[2], vec![97, 75, 47, 29, 13]);
+        assert_eq!(updates.len(), 3);
+        assert_eq!(updates[0], &[75, 97, 47, 61, 53]);
+        assert_eq!(updates[1], &[61, 13, 29]);
+        assert_eq!(updates[2], &[97, 13, 75, 29, 47]);
+    }
+
+    #[test]
+    fn solves_part_2() {
+        let answer = Day5Solver::part_2(SAMPLE);
+        assert_eq!(answer, 123);
     }
 }
-
-// pages to print: collect ordering rules
